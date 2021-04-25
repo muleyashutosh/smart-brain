4a90717fdc8180e4ba8799fe65c296539fdf3c1b
Ashutosh <muleyashutosh@gmail.com>
2021-04-25 23:43:54 +0530

    input validation and error message display on login

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,105 +1,126 @@
-import React from 'react';
+import React from "react";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 class Register extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      email: '',
-      password: ''
-    }
+      name: "",
+      email: "",
+      password: "",
+      requestFailed: false,
+      errorMessage: "",
+    };
   }
-
 
   OnNameChange = (event) => {
-    this.setState({name: event.target.value})
-  }
+    this.setState({ name: event.target.value });
+  };
 
   OnEmailChange = (event) => {
-    this.setState({email: event.target.value})
-  }
+    this.setState({ email: event.target.value });
+  };
+
+  OnInputFocus = () => {
+    this.setState({ requestFailed: false });
+  };
 
   OnPaswordChange = (event) => {
-    this.setState({password: event.target.value})
-  }
+    this.setState({ password: event.target.value });
+  };
 
-  OnRegisterSubmit = () => {
-    const {email, password, name } = this.state;
-    fetch('https://whispering-sierra-61887.herokuapp.com/register', {
-      method: 'post',
+  OnRegisterSubmit = (event) => {
+    event.preventDefault();
+    NProgress.start();
+    const { email, password, name } = this.state;
+    fetch("https://whispering-sierra-61887.herokuapp.com/register", {
+      method: "post",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         name: name,
         email: email,
-        password: password
-      })
+        password: password,
+      }),
     })
-    .then(response => response.json())
-    .then(user => {
-      if(user.id) {
-        this.props.loadUser(user);
-        this.props.OnRouteChange('home');
-      } 
-    })
-    
-  }
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.id) {
+          this.props.loadUser(user);
+          this.props.OnRouteChange("home");
+        } else {
+          this.setState({
+            requestFailed: true,
+            errorMessage: "Unable to register",
+          });
+        }
+        NProgress.done();
+      });
+  };
 
   render() {
+    const { requestFailed, errorMessage } = this.state;
     return (
       <article className="br3 shadow-5 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
         <main className="pa4 black-80">
-          <div className="measure">
+          <form className="measure" onSubmit={this.OnRegisterSubmit}>
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f3 fw6 ph0 mh0">Register</legend>
               <div className="mt3">
-                <label className="db fw6 lh-copy f5" htmlFor="name">Name</label>
-                <input 
-                  onChange = {this.OnNameChange}
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                  type="text" 
-                  name="name" 
-                  id="name" 
+                <label className="db fw6 lh-copy f5" htmlFor="name">
+                  Name
+                </label>
+                <input
+                  onChange={this.OnNameChange}
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="text"
+                  name="name"
+                  id="name"
+                  required
                 />
               </div>
               <div className="mt3">
-                <label className="db fw6 lh-copy f5" htmlFor="email-address">Email</label>
-                <input 
+                <label className="db fw6 lh-copy f5" htmlFor="email-address">
+                  Email
+                </label>
+                <input
                   onChange={this.OnEmailChange}
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                  type="email" 
-                  name="email-address" 
-                  id="email-address" 
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  required
                 />
               </div>
               <div className="mv3">
-                <label className="db fw6 lh-copy f5" htmlFor="password">Password</label>
-                <input 
+                <label className="db fw6 lh-copy f5" htmlFor="password">
+                  Password
+                </label>
+                <input
                   onChange={this.OnPaswordChange}
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                  type="password" 
-                  name="password" 
-                  id="password" 
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                  required
                 />
               </div>
             </fieldset>
+            {requestFailed ? <div className="error">{errorMessage}</div> : null}
             <div className="">
               <input
-                onClick={this.OnRegisterSubmit}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f5 dib" 
-                type="submit" 
-                value="Register" 
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f5 dib"
+                type="submit"
+                value="Register"
               />
             </div>
-          </div>
+          </form>
         </main>
       </article>
-    )
+    );
   }
-
 }
-
 
 export default Register;
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,101 +1,125 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
-import NProgress from 'nprogress';
+import React from "react";
+import NProgress from "nprogress";
 import "nprogress/nprogress.css";
-
 
 class SignIn extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      signInEmail: '',
-      signInPassword: ''
-    }
+      signInEmail: "",
+      signInPassword: "",
+      requestFailed: false,
+      errorMessage: "",
+    };
   }
 
   OnEmailChange = (event) => {
-    this.setState({signInEmail: event.target.value})
-  }
+    this.setState({ signInEmail: event.target.value });
+  };
 
   OnPaswordChange = (event) => {
-    this.setState({signInPassword: event.target.value})
-  }
+    this.setState({ signInPassword: event.target.value });
+  };
 
-  OnSignInSubmit = () => {
+  OnInputFocus = () => {
+    this.setState({ requestFailed: false });
+  };
+
+  OnSignInSubmit = (event) => {
+    event.preventDefault();
     NProgress.start();
-    const {signInEmail, signInPassword } = this.state;
-    fetch('https://whispering-sierra-61887.herokuapp.com/signin', {
-      method: 'post',
+    const { signInEmail, signInPassword } = this.state;
+    fetch("https://whispering-sierra-61887.herokuapp.com/signin", {
+      method: "post",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         email: signInEmail,
-        password: signInPassword
-      })
+        password: signInPassword,
+      }),
     })
-    .then(response => response.json())
-    .then(data => {
-      if(data.id) {
-        this.props.loadUser(data)
-        this.props.OnRouteChange('home');
-        NProgress.done()
-      } 
-    })
-
-  }
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.id) {
+          this.props.loadUser(data);
+          this.props.OnRouteChange("home");
+        } else if (data === "Wrong Credentials") {
+          this.setState({
+            requestFailed: true,
+            errorMessage: "Invalid Email/Password",
+          });
+        } else {
+          this.setState({
+            requestFailed: true,
+            errorMessage: "Error connecting to Server",
+          });
+        }
+        NProgress.done();
+      });
+  };
 
   render() {
-    const {OnRouteChange} = this.props
+    const { OnRouteChange } = this.props;
+    const { requestFailed, errorMessage } = this.state;
     return (
       <article className="br3 shadow-5 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
         <main className="pa4 black-80">
-          <div className="measure">
+          <form className="measure" onSubmit={this.OnSignInSubmit}>
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f3 fw6 ph0 mh0">Sign In</legend>
-              
               <div className="mt3">
-                <label className="db fw6 lh-copy f5" htmlFor="email-address">Email</label>
-                <input 
-                  onChange = {this.OnEmailChange}
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                  type="email" 
-                  name="email-address" 
+                <label className="db fw6 lh-copy f5" htmlFor="email-address">
+                  Email
+                </label>
+                <input
+                  onChange={this.OnEmailChange}
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
                   id="email-address"
+                  onFocus={this.OnInputFocus}
+                  required
                 />
               </div>
               <div className="mv3">
-                <label className="db fw6 lh-copy f5" htmlFor="password">Password</label>
-                <input 
-                  onChange = {this.OnPaswordChange}
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                  type="password" 
-                  name="password" 
-                  id="password" 
+                <label className="db fw6 lh-copy f5" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  onChange={this.OnPaswordChange}
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onFocus={this.OnInputFocus}
+                  required
+                  min="8"
                 />
               </div>
             </fieldset>
+            {requestFailed ? <div className="error">{errorMessage}</div> : null}
             <div className="">
               <input
-                onClick={this.OnSignInSubmit}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f5 dib" 
-                type="submit" 
-                value="Sign in" 
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f5 dib"
+                type="submit"
+                value="Sign in"
               />
             </div>
             <div className="lh-copy mt3">
               <a
-                onClick={() => OnRouteChange('register')}
+                onClick={() => OnRouteChange("register")}
                 className="f6 link dim black db pointer"
-              >Register</a>
+              >
+                Register
+              </a>
             </div>
-          </div>
+          </form>
         </main>
       </article>
-    )
+    );
   }
-
 }
-
 
 export default SignIn;
